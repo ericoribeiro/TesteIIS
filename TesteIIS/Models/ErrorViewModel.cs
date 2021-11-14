7ef67cf6076dee9499cd291803ea57ab96fd5160
EricoRibeiro <ericor25@gmail.com>
2021-11-14 10:20:16 -0300

    Add project files.

---
+++ TesteIIS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TesteIIS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
